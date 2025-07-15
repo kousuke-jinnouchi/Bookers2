90cd0d3cc423ace76dd63f0b67d7fe914a1e6098
kousuke-jinnouchi <215395544+kousuke-jinnouchi@users.noreply.github.com>
2025-07-15 12:50:51 +0000

    [Add] User一覧とUser詳細画面の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,11 +11,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    @user = current_user
     @books = Book.all 
   end
 
   def show
     @book = Book.find(params[:id]) 
+    @user = @book.user
   end
 
   def edit 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  
    devise_for :users
   root to: "homes#top"
-
   resources :books 
+  resources :users, only: [:index, :show, :edit] 
 
   get 'homes/about' => 'homes#about', as: 'about'
 end
